010375f8eea760f5faed999b9802297199cbbc42
hxvvxh <2732259311@qq.com>
2020-07-09 23:43:55 +0800

    111

--- src/main/java/com/hp/placeholder/postprocess/HpBeanFactoryPostProcessor.java
+++ src/main/java/com/hp/placeholder/postprocess/HpBeanFactoryPostProcessor.java
@@ -25,6 +25,43 @@ import java.util.Objects;
  *
  *
  * 这里对HpBeanDefin 的属性进行了修改，相关的注意事项参考HpBeanDefin
+ *
+ * 使用spring提供的注解式@Component 方式创建的BeanDefinition 是ScannedGenericBeanDefinition 里面中存放着 注解，属性，方法，单例/多例，销毁方法，等所有有关bean的定义
+ *
+ */
+
+/**
+ * 详细的看了一下 BeanDefinition是如何创建的 大致的流程是：
+ * refash()的invokeBeanFactoryPostProcessors->
+ *            invokeBeanDefinitionRegistryPostProcessors->
+ *                  ConfigurationClassPostProcessor#postProcessBeanDefinitionRegistry
+ *
+ * 这个是debug得到的流程信息
+ * org.springframework.context.support.AbstractApplicationContext#refresh
+ * org.springframework.context.support.AbstractApplicationContext#invokeBeanFactoryPostProcessors
+ * org.springframework.context.support.PostProcessorRegistrationDelegate#invokeBeanFactoryPostProcessors(org.springframework.beans.factory.config.ConfigurableListableBeanFactory, java.util.List<org.springframework.beans.factory.config.BeanFactoryPostProcessor>)
+ * org.springframework.context.support.PostProcessorRegistrationDelegate#invokeBeanDefinitionRegistryPostProcessors
+ * org.springframework.context.annotation.ConfigurationClassPostProcessor#postProcessBeanDefinitionRegistry
+ * org.springframework.context.annotation.ConfigurationClassPostProcessor#processConfigBeanDefinitions
+ *
+ * org.springframework.context.annotation.ConfigurationClassParser#parse(java.util.Set<org.springframework.beans.factory.config.BeanDefinitionHolder>)(D:/maven-reponse/org/springframework/spring-context/5.2.2.RELEASE/spring-context-5.2.2.RELEASE.jar!/org/springframework/context/annotation/ConfigurationClassPostProcessor.class:207)
+ * 		org.springframework.context.annotation.ConfigurationClassParser#parse(org.springframework.core.type.AnnotationMetadata, java.lang.String)
+ * 		org.springframework.context.annotation.ConfigurationClassParser#processConfigurationClass
+ * 		org.springframework.context.annotation.ConfigurationClassParser#doProcessConfigurationClass
+ * 		org.springframework.context.annotation.ComponentScanAnnotationParser#parse
+ * 		org.springframework.context.annotation.ClassPathBeanDefinitionScanner#doScan
+ * 			org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider#findCandidateComponents
+ * 			org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider#scanCandidateComponents
+ * 			org.springframework.core.type.classreading.CachingMetadataReaderFactory#getMetadataReader (这里根据@Springboot 注解的基础scan去加载资源)
+ * 				org.springframework.core.type.classreading.SimpleMetadataReaderFactory#getMetadataReader(org.springframework.core.io.Resource)
+ * 				org.springframework.core.type.classreading.SimpleMetadataReader#SimpleMetadataReader
+ * 				org.springframework.asm.ClassReader#accept(org.springframework.asm.ClassVisitor, int)
+ * 				org.springframework.asm.ClassReader#accept(org.springframework.asm.ClassVisitor, org.springframework.asm.Attribute[], int) ######## 这里是最终读取文件的地方，使用classloader 使用的是最基础的read 得到的是 ScannedGenericBeanDefinition的集合
+ *
+ * 		org.springframework.context.annotation.AnnotationConfigUtils#applyScopedProxyMode(D:/maven-reponse/org/springframework/spring-context/5.2.2.RELEASE/spring-context-5.2.2.RELEASE.jar!/org/springframework/context/annotation/ClassPathBeanDefinitionScanner.class:131)
+ *
+ *
+ * org.springframework.context.annotation.ConfigurationClassBeanDefinitionReader#loadBeanDefinitions
  */
 @Component
 @Slf4j
