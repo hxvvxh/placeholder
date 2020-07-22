c1113206a0ff24268c421876e16a4f04cba14cb8
hxvvxh <2732259311@qq.com>
2020-07-22 23:10:45 +0800

    1

--- src/main/java/com/hp/placeholder/interceptor/HpMethodInterceptor.java
+++ src/main/java/com/hp/placeholder/interceptor/HpMethodInterceptor.java
@@ -3,6 +3,9 @@ package com.hp.placeholder.interceptor;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
+/**
+ * aop
+ */
 public class HpMethodInterceptor implements MethodInterceptor {
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
--- src/main/java/com/hp/placeholder/interceptor/HpMethodInterceptor2.java
+++ src/main/java/com/hp/placeholder/interceptor/HpMethodInterceptor2.java
@@ -5,6 +5,9 @@ import org.springframework.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
 
+/**
+ * core
+ */
 public class HpMethodInterceptor2 implements MethodInterceptor {
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
